cae326b6e79139312a1a5da7549eeb98a0b7929e
juan0tron <juandglover@gmail.com>
2021-07-28 21:48:25 -0700

    Add gb camera pics, update glitches

--- js/index.js
+++ js/index.js
@@ -46,20 +46,29 @@ const artGallery = {
 	init: (selector) => {
 		let images = [
 			'/images/art/meryl-glitch.jpg',
-			'/images/art/meryl-3d.jpg',
+			'/images/art/LiftA-crop.jpg',
 			'/images/art/XenoGlitch3D.jpg',
-
-			'/images/art/zoewolfe-poster.jpg',
+			
 			'/images/art/owl.jpg',
-			'/images/art/thirteen-one.jpg',
-			'/images/art/cherry-hill.jpg',
+			'/images/art/meryl-3d.jpg',
+			'/images/art/MarilynMissingNo.png',
+			
 			'/images/art/Xenoglitch_2.jpg',
-
+			'/images/art/cherry-hill.jpg',
+			'/images/art/tlou-hallway.png',
+			'/images/art/thirteen-one.jpg',
+			
 			'/images/art/ludens.jpg',
-			'/images/art/alien.jpg',
+			// '/images/art/phaser.jpg',
 
-			'/images/art/ractro.jpg',
+			'/images/art/lastones-rb.jpg',
+
 			'/images/art/3d-trees.jpg',
+			'/images/art/lastones-fog.jpg',
+			'/images/art/brainmush.jpg',
+			'/images/art/LiftD-recolor.jpg',
+			
+			'/images/art/ractro.jpg',
 
 		];
 		let html = '';
@@ -95,6 +104,66 @@ const artGallery = {
 	}
 }
 
+const gameboyGallery = {
+	init: (selector) => {
+		let images = [
+			'/images/gameboycamera/photo_20_purp.png',
+			'/images/gameboycamera/photo_16_bro.png',
+			'/images/gameboycamera/gb_04_c.png',
+			'/images/gameboycamera/ghosty_1.png',
+			'/images/gameboycamera/Gradient Map 1(2).png',
+			'/images/gameboycamera/photo_7_gb.png',
+			// '/images/gameboycamera/photo_7_purp.png',
+			'/images/gameboycamera/photo_27_pink.png',
+			'/images/gameboycamera/birbhouse.png',
+			'/images/gameboycamera/raccoon_1.png',
+			'/images/gameboycamera/treez.png',
+			'/images/gameboycamera/photo_27_green.png',
+			'/images/gameboycamera/photo_23_bro.png',
+			'/images/gameboycamera/photo_19_pink.png',
+			'/images/gameboycamera/photo_25_pink.png',
+			// '/images/gameboycamera/stairs.png',
+			// '/images/gameboycamera/3tree.png',
+			'/images/gameboycamera/ghost-1.png',
+			// '/images/gameboycamera/photo_13.png',
+			// '/images/gameboycamera/photo_16_blu.png',
+			// '/images/gameboycamera/photo_16_purp.png',
+			// '/images/gameboycamera/photo_16.png',
+			// '/images/gameboycamera/trees.png',
+		];
+		let html = '';
+		for (let image of images){
+			html += gameboyGallery.image(image);
+		}
+		$(selector).html(html);
+		$(selector).lightGallery();
+		gameboyGallery.masonry(selector);
+	},
+	masonry: (selector) => {
+		let image_grid = $(selector).isotope({
+			itemSelector: '.grid-item',
+			masonry: {
+				percentPosition: true,
+				gutter: 20,
+			}
+		});
+		// Update masonry layout after each image loads
+		image_grid.imagesLoaded().progress(()=>{
+			image_grid.isotope('layout');
+		});
+	},
+	image: (image) => {
+		return `
+			<div class="grid-item" data-src="${image}" data-download-url="${image}">
+				<img src="${image}"/>
+				<div class="hover-icon">
+					<span><i class="fa fa-search"></i></span>
+				</div>
+			</div>
+		`;
+	}
+}
+
 function showHideNav(){
 
 }
@@ -102,4 +171,5 @@ function showHideNav(){
 $(document).ready(() => {
 	codeGallery.init('#project-gallery');
 	artGallery.init('#art-gallery .masonry-grid');
+	gameboyGallery.init('#gameboy-gallery .masonry-grid');
 });
